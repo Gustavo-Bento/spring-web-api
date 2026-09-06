56dd11060cca84b5b2d75ca4af9db78dba60f89b
Gustavo-Bento <gfmb94@gmail.com>
2026-09-06 02:00:39 -0300

    feat(controller): add welcome endpoint

---
+++ src/main/java/com/bento/springwebapi/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.bento.springwebapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String welcome(){
+        return "Welcome to the Spring Web API!";
+    }
+}
